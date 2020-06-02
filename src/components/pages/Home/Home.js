cdac7841395c4788a068d9483b194891680d2427
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-06-01 21:39:30 -0500

    finished routing

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.scss';
 
+import { Link } from 'react-router-dom';
+
 class Home extends React.Component {
   editEvent = (e) => {
     e.preventDefault();
@@ -13,6 +15,8 @@ class Home extends React.Component {
       <div className="Home">
         <h1>Home Component</h1>
         <button className="btn btn-dark" onClick={this.editEvent}>Edit a thing</button>
+        <Link to='/scats/klasgkjasdfkjakjsdihhirheguh'>View Single</Link>
+        <Link to='/new'>New Scat</Link>
       </div>
     );
   }
